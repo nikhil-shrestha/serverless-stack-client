c3be82c43e7e76edc70c551540047070724a6b8d
illusionist <nikhil.shrestha1995@gmail.com>
2020-05-29 18:42:59 +0545

    Add Stripe Keys to Config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export default {
+const dev = {
+  STRIPE_KEY: 'pk_test_Qzpqxw8oqfWF4vusTPrPEDs4',
   s3: {
     REGION: 'ap-south-1',
     BUCKET: 'note-app-asdfjk-uploads',
@@ -13,4 +14,31 @@ export default {
     APP_CLIENT_ID: '4rt26ofo383d4pu3ok8us4b1fm',
     IDENTITY_POOL_ID: 'ap-south-1:0d58f8e5-b244-4e64-a00f-4f732a347745',
   },
+};
+
+const prod = {
+  STRIPE_KEY: 'pk_test_Qzpqxw8oqfWF4vusTPrPEDs4',
+  s3: {
+    REGION: 'ap-south-1',
+    BUCKET: 'note-app-asdfjk-uploads',
+  },
+  apiGateway: {
+    REGION: 'ap-south-1',
+    URL: 'https://nj7doo7r23.execute-api.ap-south-1.amazonaws.com/dev',
+  },
+  cognito: {
+    REGION: 'ap-south-1',
+    USER_POOL_ID: 'ap-south-1_F6VEUumV5',
+    APP_CLIENT_ID: '4rt26ofo383d4pu3ok8us4b1fm',
+    IDENTITY_POOL_ID: 'ap-south-1:0d58f8e5-b244-4e64-a00f-4f732a347745',
+  },
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
 };
