7c0fb73f1e2718456c42c3826217a176b9c80a78
illusionist <nikhil.shrestha1995@gmail.com>
2020-05-29 15:52:37 +0545

    setting up amplify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Nav, Navbar, NavItem } from 'react-bootstrap';
 
 import Routes from './Routes';
 
@@ -9,13 +10,21 @@ import './App.css';
 function App() {
   return (
     <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
+      <Navbar fluid collapseOnSelect bg="light" expand="lg">
+        <Navbar.Brand>
+          <Link to="/">Scratch</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
       <Routes />
     </div>
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'ap-south-1',
+    BUCKET: 'note-app-asdfjk-uploads',
+  },
+  apiGateway: {
+    REGION: 'ap-south-1',
+    URL: 'https://nj7doo7r23.execute-api.ap-south-1.amazonaws.com/dev',
+  },
+  cognito: {
+    REGION: 'ap-south-1',
+    USER_POOL_ID: 'ap-south-1',
+    APP_CLIENT_ID: '4rt26ofo383d4pu3ok8us4b1fm',
+    IDENTITY_POOL_ID: 'ap-south-1:0d58f8e5-b244-4e64-a00f-4f732a347745',
+  },
+};
